4fc060d1b9c481ec736dedf16f7b83a891d753fc
Rafal Bednarz <raf.bednarz@gmail.com>
2019-05-15 22:49:50 +0200

    first commit

---
+++ src/main/java/pl/itacademy/testsportal/TestsPortalApplication.java
@@ -0,0 +1,13 @@
+package pl.itacademy.testsportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsPortalApplication.class, args);
+	}
+
+}
