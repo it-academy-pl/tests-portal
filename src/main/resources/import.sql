c408076cdaac173a9043abbaef3ac267ccba40ea
Rafal Bednarz <raf.bednarz@gmail.com>
2019-06-21 20:11:54 +0200

    basic spring security

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
-INSERT INTO STUDENT (id, email, name, surname, password) values (100, 'rubel@coin.ru', 'Staszek', 'Rubel', 'password');
+--https://www.browserling.com/tools/bcrypt - use it to generate BCrypt hashes
+INSERT INTO STUDENT (id, email, name, surname, password) values (100, 'rubel@coin.ru', 'Staszek', 'Rubel', '$2a$10$nLyxmsXDEwlRQSZOEManGeX1q.z/dh1wPDHbVkodFuMcmKeYSH5QS');
 INSERT INTO STUDENT (id,email,name,surname,password) values (101,'radek@onet.pl','Radek','Dzięcioł','password');
 INSERT INTO STUDENT (id,email,name,surname,password) values (102,'drugi@onet.pl','Piotr','Sekuła','password');
