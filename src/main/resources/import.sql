ced040c671aa4075405d5883334340a87882e36f
Rafal Bednarz <raf.bednarz@gmail.com>
2019-07-05 20:18:23 +0200

    roles added

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 --https://www.browserling.com/tools/bcrypt - use it to generate BCrypt hashes
-INSERT INTO STUDENT (id, email, name, surname, password) values (100, 'admin', 'Staszek', 'Rubel', '$2a$10$v0kjaRjDUS/GWtEFLXAmieC/WDVipcO58WQ6wkvfn0QGleyHHfv36');
+INSERT INTO STUDENT (id, email, name, surname, password) values (100, 'student@agh.edu.pl', 'Staszek', 'Rubel', '$2a$10$v0kjaRjDUS/GWtEFLXAmieC/WDVipcO58WQ6wkvfn0QGleyHHfv36');
 INSERT INTO STUDENT (id,email,name,surname,password) values (101,'radek@onet.pl','Radek','Dzięcioł','password');
 INSERT INTO STUDENT (id,email,name,surname,password) values (102,'drugi@onet.pl','Piotr','Sekuła','password');
 
@@ -8,7 +8,6 @@ INSERT INTO TASK (id,description,subject,difficulty,STUDENT_ID) values (100,'Obl
 INSERT INTO PARAMETER (id,name,value,unit,TASK_ID) values (100,'a',90,'mm',100);
 INSERT INTO PARAMETER (id,name,value,unit,TASK_ID) values (200,'b',30,'mm',100);
 
+INSERT INTO ADMIN (id,email,password) values (100,'admin@agh.edu.pl','$2a$10$nLyxmsXDEwlRQSZOEManGeX1q.z/dh1wPDHbVkodFuMcmKeYSH5QS');
 
-INSERT INTO ADMIN (id,email,password) values (100,'admin','$2a$10$nLyxmsXDEwlRQSZOEManGeX1q.z/dh1wPDHbVkodFuMcmKeYSH5QS');
-
-CREATE OR REPLACE VIEW USERS AS SELECT email, password from STUDENT UNION ALL select email, password from ADMIN;
+CREATE OR REPLACE VIEW USERS AS SELECT email, password, 'ROLE_USER' as role from STUDENT UNION ALL select email, password, 'ROLE_ADMIN' as role from ADMIN;
